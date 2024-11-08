823b956e783fb3712d8e6836b7ed0537886585b2
Ishika Patidar <ishikapatidar06012002@gmail.com>
2024-11-08 17:19:15 +0530

    added

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,6 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode, ComponentType } from "react";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
-
 import { cn } from "@/lib/utils";
 
 const BentoGrid = ({
@@ -26,7 +25,7 @@ const BentoCard = ({
   name,
   className,
   background,
-
+  Icon,
   description,
   href,
   cta,
@@ -34,7 +33,7 @@ const BentoCard = ({
   name: string;
   className: string;
   background: ReactNode;
-
+  Icon: ComponentType<{ className?: string }>;
   description: string;
   href: string;
   cta: string;
@@ -52,7 +51,7 @@ const BentoCard = ({
   >
     <div>{background}</div>
     <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-      {/* <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" /> */}
+      <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
       <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
         {name}
       </h3>
